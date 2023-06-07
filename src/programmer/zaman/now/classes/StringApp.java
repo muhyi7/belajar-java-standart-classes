c36d1d1a9a0362aa66983e2446677853ded5a959
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-06-07 18:26:32 +0700

    first commit

---
+++ src/programmer/zaman/now/classes/StringApp.java
@@ -0,0 +1,31 @@
+package programmer.zaman.now.classes;
+
+import java.util.Locale;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name = "Eko Kurniawan Khannedy";
+        String nameLowerCase = name.toLowerCase();
+        String nameUpperCase = name.toUpperCase();
+
+        System.out.println(name);
+        System.out.println(nameLowerCase);
+        System.out.println(nameUpperCase);
+        System.out.println(name.length());
+        System.out.println(name.startsWith("Eko"));
+        System.out.println(name.endsWith("Khannedy"));
+
+        String[] names = name.split(" ");
+        for(var value : names){
+            System.out.println(value);
+        }
+
+        System.out.println(" ".isBlank());
+        System.out.println(" ".isEmpty());
+        System.out.println("".isEmpty());
+        System.out.println(name.charAt(0));
+
+        char[] chars = name.toCharArray();
+
+    }
+}
